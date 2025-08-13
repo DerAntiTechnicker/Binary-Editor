89064fc685d341a083c057c2b9e35f7e9b87d501
Florian Schneck <147105863+DerAntiTechnicker@users.noreply.github.com>
2025-08-13 18:01:47 +0200

    Removed Class "CommandLine" -> currently writing own library: https://github.com/DerAntiTechnicker/Command-Line

--- main.cpp
+++ main.cpp
@@ -8,31 +8,8 @@
  ***************************************************/
 
 #include "datei.h"
-#include "CommandLine.h"
 
-#include <conio.h>
-
-void CommandLine::CommandActions() {     // later it should be only a prototype!
-        if (input == commands[i]) {     // exit
-            running = false;
-        }
-}
 
 int main() {
-    CommandLine cmdLine;
-    cmdLine.init();
-    getch();
     return 0;
 }
-
-
-
-
-
-
-
-/*std::fstream file("C:/Users/Florian Schneck/ClionProjects/Binary-Editor/Test.bin", std::ios::out | std::ios::trunc | std::ios::binary);
-    int text[1000];
-    for (int i = 0; i < 50; i++) text[i] = 24;
-    for (int i = 50; i < 100; i++) text[i] = 1024;
-    file.write(reinterpret_cast<char*>(text), sizeof(int) * 100);*/
